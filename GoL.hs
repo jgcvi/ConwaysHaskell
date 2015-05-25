6414cee31fb5062cdc3ddb5bd3268b9002dc0a45
jgcvi <coatsj11@gmail.com>
2015-05-24 20:26:38 -0700

    Done with a smarter data type

--- GoL.hs
+++ GoL.hs
@@ -1,53 +1,27 @@
 module GoL where
-import System.IO
 
-val = [[Alive, Alive], [Alive, Dead]]
-val2 = [[Alive, Dead, Alive], [Dead, Alive, Dead], [Alive,Dead,Alive]]
-
-data Cell = Dead | Alive deriving (Show, Eq)
-type Board = [[Cell]]
+data Cell = Dead | Alive 
+	deriving (Show, Eq)
+data GameCell = C (Cell, (Int, Int))
+instance Show GameCell where 
+	show (C (x,_)) = show x 
+type Board = [[GameCell]]
 
 makeMove :: Board -> Board
-makeMove = intBoardToBoard . prepareForMove 0
+makeMove = neighborBoardToBoard . getNeighborCounts
 
-intBoardToBoard :: [[(Int, Cell)]] -> Board
-intBoardToBoard = map (\x -> map intToCell x)
+neighborBoardToBoard :: [[(Int, GameCell)]] -> Board
+neighborBoardToBoard = map (\x -> map tupleToCell x)
 
-intToCell :: (Int, Cell) -> Cell
-intToCell (x,c)
-	| x == 2 && c == Alive = Alive
-	| x == 3 = Alive
-	| otherwise = Dead
+tupleToCell :: (Int, GameCell) -> GameCell
+tupleToCell (count, C (val,c))
+	| count == 2 && val == Alive = C (Alive,c)
+	| count == 3 = C (Alive,c)
+	| otherwise = C (Dead,c)
 
-prepareForMove :: Int -> Board -> [[(Int, Cell)]]
-prepareForMove y board = [convertToNeighborCounts 0 y (head $ drop y board) board] ++ rest where
-	rest 
-		| y + 1 == length board = []
-		| otherwise = prepareForMove (y+1) board
+getNeighborCounts :: Board -> [[(Int, GameCell)]] 
+getNeighborCounts b = map (map (\x -> (length (getLiveNeighbors x b), x))) b
 
-getNeighbors :: Int -> Int -> Board -> [Cell]
-getNeighbors x y board@(h:t) = xReduced `yReduce` y where
-	xReduced
-		| x == 0 = [h, head t]
-		| x + 1 == length board = [last $ init board, last board]
-		| otherwise = take 3 $ drop (x-1) board
-
-yReduce :: Board -> Int -> [Cell]
-yReduce board y = concat reduced where
-	fn = map (drop (y -1))
-	reduced
-		| y == 0 = map (take 2) board
-		| y + 1== length board = fn board
-		| otherwise = map (take 3) $ fn board
-
-convertToNeighborCounts :: Int -> Int -> [Cell] -> Board -> [(Int, Cell)]
-convertToNeighborCounts x y row@(h:t) board = (:) ((getNeighborCount x y board, h)) rest where
-	rest
-		| x + 1 == length board = []
-		| otherwise = convertToNeighborCounts (x+1) y t board
-
-getNeighborCount :: Int -> Int -> Board -> Int
-getNeighborCount x y board = (length $ filter (== Alive) (getNeighbors x y board)) - val
-	where  val 
-		| board !! x !! y == Alive = 1
-		| otherwise = 0
+getLiveNeighbors :: GameCell -> Board -> [GameCell]
+getLiveNeighbors (C (_,(x,y))) b = [vals | vals@(C (Alive, (x1, y1))) <- concat b, 
+	   x1 `elem` [(x-1)..(x+1)] && y1 `elem` [(y-1)..(y+1)] && x1 /= x && y1 /= y]
